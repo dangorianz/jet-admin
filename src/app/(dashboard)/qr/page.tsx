68af6783bdefc9a8af615b843cff86860f36f161
Daniel Gorianz <aalbaafikaa@gmail.com>
2024-10-14 17:54:52 -0400

    prueba de mi camara

--- src/app/(dashboard)/qr/page.tsx
+++ src/app/(dashboard)/qr/page.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 'use client'
-import { useEffect, useRef, useState } from 'react';
-import { Html5QrcodeScanner } from 'html5-qrcode';
+import { useRef, useState } from 'react';
+import { Html5Qrcode } from 'html5-qrcode';
 import Swal from 'sweetalert2';
 import { updateTicket } from '@/services/ticketsService';
 import { Backdrop, Button, CircularProgress } from '@mui/material';
@@ -12,6 +12,7 @@ export default function QrScann() {
   const html5QrCodeRef = useRef<any>(null); // Almacenar la referencia del escáner
   const [open, setOpen] = useState(false);
   const [isProcessing, setIsProcessing] = useState(false);
+  const [cameraStarted, setCameraStarted] = useState(false); // Nuevo estado para controlar si la cámara está activa
 
   const checkTicketStatus = async (qrDataToJson: any) => {
     if (isProcessing) {
@@ -28,21 +29,19 @@ export default function QrScann() {
         await Swal.fire({
           title: 'Entrada válida',
           icon: 'success',
-          timer: 2000,
+          timer: 1500,
           showConfirmButton: false,
-        }).then(()=>{
-          html5QrCodeRef.current?.resume(); 
-          setIsProcessing(false);
+        }).then(() => {
+          html5QrCodeRef.current?.resume();  // Reanudar el escaneo cuando el Swal finalice
         });
       } else {
         await Swal.fire({
           title: ticketUpdated?.msg,
           icon: 'error',
-          timer: 2000,
+          timer: 1500,
           showConfirmButton: false,
-        }).then(()=>{
-          html5QrCodeRef.current?.resume(); 
-          setIsProcessing(false);
+        }).then(() => {
+          html5QrCodeRef.current?.resume();  // Reanudar el escaneo cuando el Swal finalice
         });
       }
     } catch (error) {
@@ -51,12 +50,13 @@ export default function QrScann() {
       await Swal.fire({
         title: 'Error al escanear QR',
         icon: 'error',
-        timer: 2000,
+        timer: 1500,
         showConfirmButton: false,
-      }).then(()=>{
-        setIsProcessing(false);
-        html5QrCodeRef.current?.resume(); 
+      }).then(() => {
+        html5QrCodeRef.current?.resume();  // Reanudar el escaneo cuando el Swal finalice
       });
+    } finally {
+      setIsProcessing(false);
     }
   };
 
@@ -64,19 +64,20 @@ export default function QrScann() {
     setOpen(false);
   };
 
-  useEffect(() => {
-    if (qrCodeRegionRef.current) {
-      const html5QrCode = new Html5QrcodeScanner(
-        'qr-reader', 
-        { fps: 10, qrbox: { width: 250, height: 250 } },
-        false
-      );
+  const startCamera = () => {
+    if (qrCodeRegionRef.current && !cameraStarted) {
+      const html5QrCode = new Html5Qrcode("qr-reader");
 
       html5QrCodeRef.current = html5QrCode;  // Guardar la referencia
 
       // Iniciar la cámara y el escaneo de QR
-      html5QrCode.render(
-        async (decodedText: string, decodedResult: any) => {
+      html5QrCode.start(
+        { facingMode: "environment" }, // Usar la cámara trasera
+        {
+          fps: 10,    // Fotogramas por segundo
+          qrbox: { width: 250, height: 250 }  // Área de escaneo
+        },
+        async (decodedText: string) => {
           if (!isProcessing) {
             setIsProcessing(true);
             html5QrCode.pause();  // Pausar el escaneo temporalmente
@@ -87,19 +88,26 @@ export default function QrScann() {
         (error: any) => {
           console.warn(`Error de escaneo: ${error}`);
         }
-      )
+      ).catch((err: any) => {
+        setCameraStarted(false)
+        console.error(`Error iniciando el escaneo: ${err}`);
+      });
 
-      return () => {
-        // No limpiar ni detener la cámara aquí para mantenerla activa
-      };
+      setCameraStarted(true);  // Marcar que la cámara ha sido activada
     }
-  }, [isProcessing]);
+  };
 
   return (
     <>
       <div className='w-full max-h-screen'>
         <div className='flex justify-center py-6 font-bold'>
-          {!isProcessing ? <p>Listo para scanear</p> : <Button onClick={() => html5QrCodeRef.current?.resume()}> Escanear otro QR </Button>}
+          {!cameraStarted ? (
+            <Button variant="contained" onClick={startCamera}>
+              Activar Cámara para Escanear QR
+            </Button>
+          ) : (
+            !isProcessing ? <p>Listo para escanear</p> : <p>Procesando...</p>
+          )}
         </div>
         <div id="qr-reader" ref={qrCodeRegionRef}></div>
       </div>
