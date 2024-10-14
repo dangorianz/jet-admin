c1babd1ad72d91f9c6e8fdcba5b3350b8046da3f
Daniel Gorianz <aalbaafikaa@gmail.com>
2024-10-14 17:30:06 -0400

    prueba 2 scanner 2

--- src/app/(dashboard)/qr/page.tsx
+++ src/app/(dashboard)/qr/page.tsx
@@ -3,38 +3,39 @@
 'use client'
 import { useEffect, useRef, useState } from 'react';
 import { Html5QrcodeScanner } from 'html5-qrcode';
-import _ from 'lodash';
 import Swal from 'sweetalert2';
 import { updateTicket } from '@/services/ticketsService';
 import { Backdrop, Button, CircularProgress } from '@mui/material';
 
 export default function QrScann() {
   const qrCodeRegionRef = useRef<HTMLDivElement>(null);
-
+  const qrCodeScannerRef = useRef<any>(null); // Almacenar la referencia del escáner
   const [open, setOpen] = useState(false);
-  const [isProcessing, setIsProcessing] = useState(false);  // Añadido estado para bloqueo
+  const [isProcessing, setIsProcessing] = useState(false);
 
-  const checkTicketStatus  = async (qrDataToJson:any) => {
+  const checkTicketStatus = async (qrDataToJson: any) => {
     if (isProcessing) {
       return;
     }
-    setIsProcessing(true)
+    setIsProcessing(true);
     setOpen(true);
+
     try {
       const ticketUpdated = await updateTicket(qrDataToJson);
       setOpen(false);
+
       if (ticketUpdated?.ok) {
         await Swal.fire({
-          title:'Entrada válida',
-          icon:'success',
-          timer:1500,
+          title: 'Entrada válida',
+          icon: 'success',
+          timer: 1500,
           showConfirmButton: false,
         });
       } else {
         await Swal.fire({
           title: ticketUpdated?.msg,
-          icon:'error',
-          timer:1500,
+          icon: 'error',
+          timer: 1500,
           showConfirmButton: false,
         });
       }
@@ -42,11 +43,14 @@ export default function QrScann() {
       setOpen(false);
       console.log('error', error);
       await Swal.fire({
-        title:'Error al escanear QR',
-        icon:'error',
-        timer:1500,
+        title: 'Error al escanear QR',
+        icon: 'error',
+        timer: 1500,
         showConfirmButton: false,
       });
+    } finally {
+      setIsProcessing(false);
+      qrCodeScannerRef.current?.resume();  // Reanudar el escaneo
     }
   };
 
@@ -59,15 +63,18 @@ export default function QrScann() {
       const qrCodeScanner = new Html5QrcodeScanner(
         'qr-reader', 
         { fps: 10, qrbox: { width: 250, height: 250 } },
-        false  // El tercer argumento debe ser false para evitar que el escáner limpie la cámara
+        false
       );
+
+      qrCodeScannerRef.current = qrCodeScanner;  // Guardar el escáner en la referencia
 
       qrCodeScanner.render(
         async (decodedText: string, decodedResult: any) => {
-          if (!isProcessing) {  // Solo procesar si no está bloqueado
+          if (!isProcessing) {
             setIsProcessing(true);
+            qrCodeScanner.pause();  // Pausar el escaneo temporalmente
             const qrDataToJson = JSON.parse(decodedText);
-            await checkTicketStatus(qrDataToJson); // Procesar los datos escaneados
+            await checkTicketStatus(qrDataToJson);
           }
         },
         (error: any) => {
@@ -79,17 +86,15 @@ export default function QrScann() {
         qrCodeScanner.clear(); 
       };
     }
-  }, [isProcessing]);  // Dependencia de isProcessing para que solo permita escanear cuando está desbloqueado
-
+  }, [isProcessing]);
 
   return (
     <>
       <div className='w-full max-h-screen'>
         <div className='flex justify-center py-6 font-bold'>
-          {!isProcessing ? <p>Listo para scanear</p> : <Button onClick={() => setIsProcessing(false)}> Escanear otro QR </Button> }
-
+          {!isProcessing ? <p>Listo para scanear</p> : <Button onClick={() => qrCodeScannerRef.current?.resume()}> Escanear otro QR </Button>}
         </div>
-          <div id="qr-reader" ref={qrCodeRegionRef}></div>
+        <div id="qr-reader" ref={qrCodeRegionRef}></div>
       </div>
       <Backdrop
         sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
